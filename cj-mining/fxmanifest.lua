b0c91b773942e507006fb1ea1c434e13380d2e9a
CarlosJhn <149292807+CarlosJhn@users.noreply.github.com>
2023-11-09 02:14:07 +0700

    Add files via upload

---
+++ cj-mining/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'carlosjhn#0'
+description 'Basic Mining Job For QB'
+version '1.0'
+
+client_scripts {'client/*.lua'}
+server_scripts {'server/*.lua'}
+shared_scripts {'@ox_lib/init.lua', 'shared/*.lua'}
